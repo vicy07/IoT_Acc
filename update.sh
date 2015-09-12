2935b479300d91e824f1b2514f30d62971d55ed8
Vicy07 <v.jesilevskis@gmail.com>
2015-09-12 22:16:14 +0300

    Fix updater to match latest code

--- update.sh
+++ update.sh
@@ -1,13 +1,14 @@
 rm -rf github
 mkdir github
-cd github
+cd ~/IoT_At_Home/github
 git clone https://github.com/vicy07/IoT_At_Home
+cd ~/IoT_At_Home
 rsync -av --progress github/IoT_At_Home/cgateway/ cgateway --exclude config.json
 cd ..
 cd cgateway
 cd RF24
 sudo make install
-cd ~/IoT_Acc/cgateway
+cd ~/IoT_At_Home/cgateway
 make
 chmod 711 gw.py
 chmod 711 remote
@@ -15,7 +16,7 @@ chmod 711 register.py
 chmod 711 init.py
 chmod 711 remote.cpp
 chmod 711 launcher.sh
-cd ~/IoT_Acc/
+cd ~/IoT_At_Home
 cp github/IoT_At_Home/update.sh update.sh -f -u
 chmod 711 update.sh
 
