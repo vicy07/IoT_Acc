716c0cb49f45bb86b18f54d53f17358b5884e3fc
Vicy07 <v.jesilevskis@gmail.com>
2015-10-06 22:29:12 +0300

    update script for Python Gateway

--- update.sh
+++ update.sh
@@ -1,21 +1,12 @@
-rm -rf github
+sudo rm -rf github
 mkdir github
-cd ~/IoT_At_Home/github
+cd ~/IoT/github
 git clone https://github.com/vicy07/IoT_At_Home
-cd ~/IoT_At_Home
-rsync -av --progress ~/IoT_At_Home/github/IoT_At_Home/cgateway/  ~/IoT_At_Home/cgateway --exclude config.json
-cd ~/IoT_At_Home/cgateway
-cd RF24
-sudo make install
-cd ~/IoT_At_Home/cgateway
-make
-chmod 711 gw.py
-chmod 711 remote
-chmod 711 register.py
-chmod 711 init.py
-chmod 711 remote.cpp
-chmod 711 launcher.sh
-cd ~/IoT_At_Home
+cd ~/IoT
+rsync -av --progress ~/IoT/github/IoT_At_Home/Gateway/  ~/IoT/Gateway --exclude config.json
+cd ~/IoT/Gateway
+chmod 711 smart_gateway.py
+cd ~/IoT
 cp github/IoT_At_Home/update.sh update.sh -f -u
 chmod 711 update.sh
 
